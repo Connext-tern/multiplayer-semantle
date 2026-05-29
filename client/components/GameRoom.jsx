14b61fce60c7abbb5dff64b6ee4f16db407c4b50
Connext-tern <suchan.hwang@connext.co.kr>
2026-05-29 16:16:07 +0900

    connect frontend to production backend

--- client/components/GameRoom.jsx
+++ client/components/GameRoom.jsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react"
 import { io } from "socket.io-client"
 
-const socket = io("http://localhost:4000")
+const socket = io("https://multiplayer-semantle.onrender.com")
 
 export default function GameRoom({ roomId }) {
   const [username, setUsername] = useState("")
